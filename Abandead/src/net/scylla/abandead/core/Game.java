5176986f78702610632ddbb239ed121cfaf78139
TankNast <lolwut642@gmail.com>
2012-03-23 16:47:30 -0700

    Made the screen!

--- Abandead/src/net/scylla/abandead/core/Game.java
+++ Abandead/src/net/scylla/abandead/core/Game.java
@@ -1,5 +1,32 @@
 package net.scylla.abandead.core;
 
-public class Game {
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()) {
+			Display.update();
+		}
+		
+		Display.destroy();
+		
+	}
+	
 }
