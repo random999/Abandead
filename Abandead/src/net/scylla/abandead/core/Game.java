b8101639ba5171c375d3b2457fbb851a87932c17
TankNast <lolwut642@gmail.com>
2012-03-25 16:51:17 -0700

    making tiles now...

--- Abandead/src/net/scylla/abandead/core/Game.java
+++ Abandead/src/net/scylla/abandead/core/Game.java
@@ -6,6 +6,8 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 import static org.lwjgl.opengl.GL11.*;
 
 public class Game {
@@ -13,6 +15,9 @@ public class Game {
 	private int xScroll = 0;
 	private int yScroll = 0;
 	private int frameCount = 0;
+	
+	private static final int TILE_SIZE = 32;
+	private static final int MAP_SIZE = 4;
 	
 	private long prevTime = (Sys.getTime()*1000)/Sys.getTimerResolution();
 
@@ -49,6 +54,15 @@ public class Game {
 		System.out.println("Exiting game...");
 		Display.destroy();
 	}
+	
+	private void draw() {
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+		drawBackground();
+		drawSprite();
+		Display.update();
+	}
 
 	private void pollInput() {
 
@@ -64,19 +78,19 @@ public class Game {
 		}
 		
 		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
-			yScroll--;
-		}
-		
-		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
 			yScroll++;
 		}
 		
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			yScroll--;
+		}
+		
 		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-			xScroll--;
+			xScroll++;
 		}
 		
 		if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-			xScroll++;
+			xScroll--;
 		}
 
 	}
@@ -116,9 +130,13 @@ public class Game {
 		}
 	}
 
-	private void draw() {
+	private void drawBackground() {
 		
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		for(int i=0; i<MAP_SIZE; i++) {
+			
+		}
+		
+		glLoadIdentity();
 		
 		if(xScroll > Display.getWidth()) {
 			xScroll = 0;
@@ -133,37 +151,66 @@ public class Game {
 		}
 		
 		if(yScroll+200 > Display.getHeight()) {
+			glLoadIdentity();
+			glTranslatef(xScroll, 0f, 0f);
 			glColor3f(0.5f, 0.5f, 1.0f);
 			glBegin(GL_QUADS);
-				glVertex2d(xScroll, 0);
-				glVertex2d(200+xScroll, 0);
-				glVertex2d(200+xScroll, yScroll-Display.getHeight()+200);
-				glVertex2d(xScroll, yScroll-Display.getHeight()+200);
+				glVertex2d(0, 0);
+				glColor3f(1.0f, 0.0f, 0.0f);
+				glVertex2d(200, 0);
+				glColor3f(0.0f, 1.0f, 0.0f);
+				glVertex2d(200, yScroll-Display.getHeight()+200);
+				glColor3f(0.0f, 0.0f, 1.0f);
+				glVertex2d(0, yScroll-Display.getHeight()+200);
 			glEnd();
 		}
 		
 		if(xScroll+200 > Display.getWidth()) {
+			glLoadIdentity();
 			glColor3f(0.5f, 0.5f, 1.0f);
+			glTranslatef(0f, yScroll, 0f);
 			glBegin(GL_QUADS);
-				glVertex2d(0, yScroll);
-				glVertex2d(200+xScroll-Display.getWidth(), yScroll);
-				glVertex2d(200+xScroll-Display.getWidth(), yScroll+200);
-				glVertex2d(0, yScroll+200);
+				glVertex2d(0, 0);
+				glColor3f(1.0f, 0.0f, 0.0f);
+				glVertex2d(200+xScroll-Display.getWidth(), 0);
+				glColor3f(0.0f, 1.0f, 0.0f);
+				glVertex2d(200+xScroll-Display.getWidth(), 200);
+				glColor3f(0.0f, 0.0f, 1.0f);
+				glVertex2d(0, 200);
 			glEnd();
 		}
 		
 		glColor3f(0.5f, 0.5f, 1.0f);
+		glLoadIdentity();
 		glBegin(GL_QUADS);
 			glVertex2d(xScroll, yScroll);
+			glColor3f(1.0f, 0.0f, 0.0f);
 			glVertex2d(200+xScroll, yScroll);
+			glColor3f(0.0f, 1.0f, 0.0f);
 			glVertex2d(200+xScroll, 200+yScroll);
+			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex2d(xScroll, 200+yScroll);
 		glEnd();
 
 		//xScroll++;
 		//yScroll++;
-		
-		Display.update();
+		glLoadIdentity();
+		glBegin(GL_LINES);
+			glVertex2d(xScroll, 0);
+			glVertex2d(xScroll, Display.getHeight());
+			glVertex2d(0, yScroll);
+			glVertex2d(Display.getWidth(), yScroll);
+			glVertex2d(xScroll+200, 0);
+			glVertex2d(xScroll+200, Display.getHeight());
+			glVertex2d(0, yScroll+200);
+			glVertex2d(Display.getWidth(), yScroll+200);
+		glEnd();
+		glBegin(GL_LINE_LOOP);
+			glVertex2d(0,0);
+			glVertex2d(Display.getWidth(), 0);
+			glVertex2d(Display.getWidth(), Display.getHeight());
+			glVertex2d(0, Display.getHeight());
+		glEnd();
 	}
 	
 	private void outputFPS() {
@@ -176,5 +223,17 @@ public class Game {
 			prevTime = newTime;
 		}
 		frameCount++;	
+	}
+	
+	private void drawSprite() {
+		glLoadIdentity();
+		glTranslatef(Display.getWidth()/2, Display.getHeight()/2, 0.0f);
+		glRotatef(xScroll, 0.0f, 0.0f, 1.0f);
+		glColor3f(0.2f, 0.7f, 0.3f);
+		glBegin(GL_TRIANGLES);
+			glVertex2d(-10, -15);
+			glVertex2d(10, -15);
+			glVertex2d(0, 15);
+		glEnd();
 	}
 }
