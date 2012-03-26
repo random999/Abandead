406e92fb1e33100ab7a221de45835823dd81b889
TankNast <lolwut642@gmail.com>
2012-03-26 07:54:49 -0700

    Begin the texture mapping!

---
+++ Abandead/src/net/scylla/abandead/core/Map.java
@@ -0,0 +1,57 @@
+package net.scylla.abandead.core;
+
+import static org.lwjgl.opengl.GL11.GL_LINES;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glVertex2d;
+
+import org.newdawn.slick.opengl.Texture;
+
+public class Map {
+
+	private static final float TILE_SIZE = Game.TILE_SIZE;
+	private static final float MAP_WIDTH = Game.MAP_WIDTH;
+	private static final float MAP_HEIGHT = Game.MAP_WIDTH;
+
+	private Texture tex;
+
+	public Map(Texture texture) {
+		this.tex = texture;
+	}
+
+	public void create(float xScroll, float yScroll) {
+
+		glLoadIdentity();
+		/*
+		 * if (xScroll > MAP_WIDTH * TILE_SIZE) { xScroll = 0; } else if
+		 * (xScroll < 0) { xScroll = (int) MAP_WIDTH * (int) TILE_SIZE; }
+		 * 
+		 * if (yScroll > MAP_HEIGHT * TILE_SIZE) { yScroll = 0; } else if
+		 * (yScroll < 0) { yScroll = (int) MAP_HEIGHT * (int) TILE_SIZE; }
+		 */
+		glBegin(GL_LINES);
+		glVertex2d(xScroll, 0);
+		glVertex2d(xScroll, 200);
+		glEnd();
+		for (float x = 0; x < MAP_WIDTH; x++) {
+			for (float y = 0; y < MAP_HEIGHT; y++) {
+				glLoadIdentity();
+				// glTranslatef(x*TILE_SIZE,y*TILE_SIZE,0f);
+				float R = x / MAP_WIDTH;
+				float G = y / MAP_HEIGHT;
+				glColor3f(R, G, G);
+				if (1 == 1) {
+					QuadTile tile = new QuadTile(x * TILE_SIZE + xScroll, y
+							* TILE_SIZE + yScroll, tex);
+				} else {
+					QuadTile tile = new QuadTile(x * TILE_SIZE + xScroll, y
+							* TILE_SIZE + yScroll, TILE_SIZE);
+				}
+			}
+		}
+
+	}
+
+}
