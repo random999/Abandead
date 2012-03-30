fb5ec024cdb35cad1b4b71686e5e132cbc77b022
TightMrKI113r <jaspermac98@yahoo.co.uk>
2012-03-30 09:19:10 +0100

    Tried to work on a usable GUI system (Added it to a new package).
    I have the main methods working, but whenever I activate the 'F' key it
    appears to run the command multiple times. I left TODO markers at all
    the Gui stuff, hopefully you might find the problem.

---
+++ Abandead/src/net/scylla/abandead/gui/InGameGui.java
@@ -0,0 +1,21 @@
+package net.scylla.abandead.gui;
+
+import javax.swing.JFrame;
+
+public class InGameGui extends Gui {
+
+	// TODO
+
+	public InGameGui() {
+		visible = true;
+	}
+
+	public void initializeGui() {
+		System.out.println("Initialized In Game GUI");
+	}
+
+	public void destroyGui() {
+		System.out.println("Destroyed In Game GUI");
+	}
+
+}
